66cb9d8465d1a662d45058c99149eec6ac9c9312
Ettore Di Giacinto <mudler@sabayon.org>
2021-04-17 10:44:44 +0200

    Fixup package tree listing

--- scripts/generate_all_provides.sh
+++ scripts/generate_all_provides.sh
@@ -3,11 +3,13 @@
 PKG_LIST=$(luet tree pkglist -o json)
 export GENERATE_PROVIDES=true
 
-for i in $(echo "$PKG_LIST" | jq -r '.packages[].path'); do
-    PACKAGE_PATH=$i
-    PACKAGE_NAME=$(echo "$PKG_LIST" | jq -r ".packages[] | select(.path==\"$i\").name")
-    PACKAGE_CATEGORY=$(echo "$PKG_LIST" | jq -r ".packages[] | select(.path==\"$i\").category")
-    PACKAGE_VERSION=$(echo "$PKG_LIST" | jq -r ".packages[] | select(.path==\"$i\").version")
+for i in $(echo "$PKG_LIST" | jq -rc '.packages[]'); do
+
+    PACKAGE_PATH=$(echo "$i" | jq -r ".path")
+    PACKAGE_NAME=$(echo "$i" | jq -r ".name")
+    PACKAGE_CATEGORY=$(echo "$i" | jq -r ".category")
+    PACKAGE_VERSION=$(echo "$i" | jq -r ".version")
+
     if [ "$PACKAGE_CATEGORY/$PACKAGE_NAME" == "layer/gentoo-stage3" ]; then
         continue
     fi
