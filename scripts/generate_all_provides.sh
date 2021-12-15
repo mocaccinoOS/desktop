02b517fbcdb404512b459b73dcb37e89d317f226
Ettore Di Giacinto <mudler@users.noreply.github.com>
2021-12-16 00:59:51 +0100

    ci: skip buildbase in provides generation

--- scripts/generate_all_provides.sh
+++ scripts/generate_all_provides.sh
@@ -9,7 +9,11 @@ for i in $(echo "$PKG_LIST" | jq -rc '.packages[]'); do
     PACKAGE_NAME=$(echo "$i" | jq -r ".name")
     PACKAGE_CATEGORY=$(echo "$i" | jq -r ".category")
     PACKAGE_VERSION=$(echo "$i" | jq -r ".version")
-
+    
+    # Skip buildbase
+    if [ "$PACKAGE_CATEGORY" == "buildbase" ]; then
+        continue
+    fi
     # Skip images/portage
     if [ "$PACKAGE_CATEGORY/$PACKAGE_NAME" == "images/portage" ]; then
         continue
