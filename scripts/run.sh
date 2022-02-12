6c864ee77bffc06e900bd13593eb27cfb0955fab
Ettore Di Giacinto <mudler@sabayon.org>
2022-02-12 21:27:38 +0100

    Add revbump scripts

---
+++ scripts/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+root=$(git rev-parse --show-toplevel)
+docker build -t desktop-scripts $root/scripts
+
+script=$1
+shift
+
+set -ex
+
+docker run -v $root:/work --workdir /work --entrypoint /bin/bash -ti --rm desktop-scripts -c "bash scripts/$script $@"
