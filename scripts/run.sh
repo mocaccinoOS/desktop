4ab773accc83fb60da5007221f72288764b77804
mudler <mudler@c3os.io>
2022-04-30 22:31:55 +0200

    scripts: no need of -ti here

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,4 @@ shift
 
 set -ex
 
-docker run -v $root:/work --workdir /work --entrypoint /bin/bash -ti --rm desktop-scripts -c "bash scripts/$script $@"
+docker run -v $root:/work --workdir /work --entrypoint /bin/bash --rm desktop-scripts -c "bash scripts/$script $@"
