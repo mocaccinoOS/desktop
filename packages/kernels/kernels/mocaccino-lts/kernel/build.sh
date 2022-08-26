be71e8dc625395a9b69f3820d4f6ebd5a1f2bc0e
Joost Ruis <joost.ruis@mocaccino.org>
2022-08-26 22:43:19 +0200

    fix arch on LTS

--- packages/kernels/kernels/mocaccino-lts/kernel/build.sh
+++ packages/kernels/kernels/mocaccino-lts/kernel/build.sh
@@ -3,6 +3,11 @@ PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
 mkdir -p output/boot
 pushd ${KERNEL_TYPE}
 
+# $ARCH can't be amd64 here, x86_64 is used here
+if [ $ARCH == "amd64" ]; then
+  ARCH = "x86_64"
+fi
+
 make -j$(nproc --ignore=1) KBUILD_BUILD_VERSION="$PACKAGE_VERSION-Mocaccino"
 
 if [[ -L "arch/${ARCH}/boot/bzImage" ]]; then
