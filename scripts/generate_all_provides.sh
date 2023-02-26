fe386e1b5bb7578f345ffecc6b119632a8f31da0
Joost Ruis <joost.ruis@mocaccino.org>
2023-02-26 15:16:38 +0100

    update provides

--- scripts/generate_all_provides.sh
+++ scripts/generate_all_provides.sh
@@ -9,7 +9,7 @@ for i in $(echo "$PKG_LIST" | jq -rc '.packages[]'); do
     PACKAGE_NAME=$(echo "$i" | jq -r ".name")
     PACKAGE_CATEGORY=$(echo "$i" | jq -r ".category")
     PACKAGE_VERSION=$(echo "$i" | jq -r ".version")
-    
+
     # Skip buildbase
     if [ "$PACKAGE_CATEGORY" == "buildbase" ]; then
         continue
