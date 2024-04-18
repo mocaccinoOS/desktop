bfb2570a53d66bd5fcbc9fea0931f3281aef8416
Joost Ruis <joost.ruis@mocaccino.org>
2024-04-19 01:00:49 +0200

    mocaccino/live-setup: detect cosmic in wayland-sessions

--- packages/mocaccino/live-setup/live-functions.sh
+++ packages/mocaccino/live-setup/live-functions.sh
@@ -283,6 +283,11 @@ prepare() {
         systemctl enable "lightdm"
     fi
 
+    if [ -f "/usr/share/wayland-sessions/cosmic.desktop" ]; then
+       	setup_default_xsession "cosmic"
+       	systemctl enable "lightdm"
+    fi
+
     if [ -f "/usr/share/xsessions/xfce.desktop" ]; then
         setup_default_xsession "xfce"
         systemctl enable "lightdm"
