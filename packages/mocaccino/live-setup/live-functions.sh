df7b6562677b8b7ede21161385af2824f6a8a1e2
Joost Ruis <joost.ruis@mocaccino.org>
2025-03-23 15:48:04 +0100

    Revert "mocaccino/live-setup: enable flathub if flatpak is installed"
    This reverts commit 5696791aa2990661d1808550e211caa920728225.

--- packages/mocaccino/live-setup/live-functions.sh
+++ packages/mocaccino/live-setup/live-functions.sh
@@ -282,41 +282,6 @@ setup_networkmanager() {
     systemctl enable ModemManager
 }
 
-enable_flathub() {
-
-    if ! command -v flatpak &> /dev/null; then
-        echo "Flatpak is not installed. Please install Flatpak first."
-        return 1  # Exit if Flatpak is not installed
-    fi
-    
-    # Path to the flatpak repo config file
-    FLATPAK_CONFIG_FILE="/var/lib/flatpak/repo/config"
-
-    # Check if flathub is already present in the config
-    if ! grep -q '\[remote "flathub"\]' "$FLATPAK_CONFIG_FILE"; then
-        echo "Flathub repository not found, adding it..."
-
-        # Append Flathub configuration to the file
-        cat >> "$FLATPAK_CONFIG_FILE" <<EOF
-
-[remote "flathub"]
-url=https://dl.flathub.org/repo/
-xa.title=Flathub
-gpg-verify=true
-gpg-verify-summary=true
-xa.comment=Central repository of Flatpak applications
-xa.description=Central repository of Flatpak applications
-xa.icon=https://dl.flathub.org/repo/logo.svg
-xa.homepage=https://flathub.org/
-EOF
-
-        # Inform the user that the remote was added
-        echo "Flathub repository added successfully."
-    else
-        echo "Flathub repository is already enabled."
-    fi
-}
-
 prepare() {
     # Try to prevent rebuild dynamic linker cache.
     # Simular case https://patchwork.openembedded.org/patch/143523/
@@ -370,5 +335,4 @@ prepare() {
     fi
 
     setup_networkmanager
-    enable_flathub
 }
