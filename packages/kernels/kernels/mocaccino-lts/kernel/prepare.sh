f509ebe4d135a2351706b5961a0295d9b2b9c7f5
Joost Ruis <joost.ruis@mocaccino.org>
2025-09-01 09:17:07 +0200

    kernels/mocaccino-lts: bump test

--- packages/kernels/kernels/mocaccino-lts/kernel/prepare.sh
+++ packages/kernels/kernels/mocaccino-lts/kernel/prepare.sh
@@ -6,6 +6,16 @@ tar xvJf kernel.tar.xz
 mv ${KERNEL_TYPE}-${PACKAGE_VERSION} ${KERNEL_TYPE}
 cp -rfv mocaccino-$ARCH.config ${KERNEL_TYPE}/.config
 cd ${KERNEL_TYPE}
+
+# --- Apply Bug 220484 patch ---
+PATCH_FILE="../patches/0001-net-ipv4-route-reset-fi-broadcast.patch"
+if [ -f "$PATCH_FILE" ]; then
+    echo "Applying network regression fix (Bug 220484)..."
+    patch -p1 < "$PATCH_FILE"
+else
+    echo "Warning: Patch file not found: $PATCH_FILE"
+fi
+
 make olddefconfig
 touch /etc/passwd
 chmod 644 /etc/passwd
