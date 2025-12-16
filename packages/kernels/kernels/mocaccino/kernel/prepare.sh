54c05e159c96c68ef77728ff0168d9c8ff376e42
Joost Ruis <joost.ruis@mocaccino.org>
2025-12-16 20:13:08 +0100

    kernels/mocaccino: bump test

--- packages/kernels/kernels/mocaccino/kernel/prepare.sh
+++ packages/kernels/kernels/mocaccino/kernel/prepare.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Set compiler FIRST
+export CC=gcc-${GCC_VERSION}
+export CXX=g++-${GCC_VERSION}
+export HOSTCC=gcc-${GCC_VERSION}
+export HOSTCXX=g++-${GCC_VERSION}
+
 set -ex
 PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
 wget https://cdn.kernel.org/pub/linux/kernel/v${PACKAGE_VERSION:0:1}.x/${KERNEL_TYPE}-${PACKAGE_VERSION}.tar.xz -O kernel.tar.xz
@@ -15,6 +21,14 @@ cd ${KERNEL_TYPE}
 # else
 #     echo "Warning: Patch file not found: $PATCH_FILE"
 # fi
+
+# Verify compiler before config generation
+echo "=== Compiler Check ==="
+${CC} --version
+echo "======================"
+
+# Clean any cached compiler detection
+make clean
 
 make olddefconfig
 touch /etc/passwd
