1f3b330567c86da000a8d6ffb4dba589787d1a5c
joost_op <joost.ruis@mocaccino.org>
2025-12-16 13:40:17 +0000

    kernels/mocaccino: bump test

--- packages/kernels/kernels/mocaccino/kernel/build.sh
+++ packages/kernels/kernels/mocaccino/kernel/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "Using CC=$(which ${CC})"
 PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
 MAJOR_VERSION=$(awk -F. '{print $1"."$2}' <<< $PACKAGE_VERSION)
 mkdir -p output/boot
 pushd ${KERNEL_TYPE}
-
+echo "Compiler version check:"
+${CC:-gcc} --version
+echo "---"
 make -j$(nproc --ignore=1) KBUILD_BUILD_VERSION="$PACKAGE_VERSION-Mocaccino"
 
 # $ARCH can't be amd64 here, x86_64 is used here
