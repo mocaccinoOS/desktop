e772a46aea112d7c79d80da71205a83c1efcd746
joost_op <joost.ruis@mocaccino.org>
2025-12-17 12:19:58 +0000

    kernels/mocacino-lts: move to gcc-15 and improve scripts

--- packages/kernels/kernels/mocaccino-lts/kernel/build.sh
+++ packages/kernels/kernels/mocaccino-lts/kernel/build.sh
@@ -3,12 +3,23 @@ PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
 mkdir -p output/boot
 pushd ${KERNEL_TYPE}
 
+echo "Compiler version check:"
+${CC:-gcc} --version
+echo "---"
+
+# make -j$(nproc --ignore=1) KBUILD_BUILD_VERSION="$PACKAGE_VERSION-Mocaccino"
+
+# Force compiler on make command line
+make -j$(nproc --ignore=1) \
+    CC="${CC}" \
+    HOSTCC="${HOSTCC}" \
+    HOSTCXX="${HOSTCXX}" \
+    KBUILD_BUILD_VERSION="$PACKAGE_VERSION-Mocaccino"
+
 # $ARCH can't be amd64 here, x86_64 is used here
 if [ $ARCH == "amd64" ]; then
   ARCH = "x86_64"
 fi
-
-make -j$(nproc --ignore=1) KBUILD_BUILD_VERSION="$PACKAGE_VERSION-Mocaccino"
 
 if [[ -L "arch/${ARCH}/boot/bzImage" ]]; then
    cp -rfv $(readlink -f "arch/${ARCH}/boot/bzImage") ../output/boot/"kernel-${KERNEL_PREFIX}-${ARCH}-${PACKAGE_VERSION}-${SUFFIX}"
