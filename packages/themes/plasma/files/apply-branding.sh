03f4d28f84ab5e50c4b05fdcc96aa89c242c3856
Joost Ruis <joost.ruis@mocaccino.org>
2026-06-22 14:30:09 +0200

    themes/plasma: bump

--- packages/themes/plasma/files/apply-branding.sh
+++ packages/themes/plasma/files/apply-branding.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-# symlinks for start menu
+
+# Install Look and Feel package
+install -Dm644 files/lookandfeel/org.mocaccino.desktop/metadata.json \
+  /usr/share/plasma/look-and-feel/org.mocaccino.desktop/metadata.json
+install -Dm644 files/lookandfeel/org.mocaccino.desktop/contents/defaults \
+  /usr/share/plasma/look-and-feel/org.mocaccino.desktop/contents/defaults
+
+# Symlinks for start menu icon
 for theme in breeze breeze-dark; do
   for size in 16 22 24; do
     dir="/usr/share/icons/${theme}/places/${size}"
@@ -9,4 +16,4 @@ for theme in breeze breeze-dark; do
   done
 done
 
-# TODO: wallpaper is still handled through a file in skel 
+# TODO: wallpaper is still handled through a file in skel
