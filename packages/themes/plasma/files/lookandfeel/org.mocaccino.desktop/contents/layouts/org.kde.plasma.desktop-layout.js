7557b3482fa9befc659ab761b8b12065d85d9bbc
Joost Ruis <joost.ruis@mocaccino.org>
2026-06-23 16:49:27 +0200

    themes/plasma: bump

--- packages/themes/plasma/files/lookandfeel/org.mocaccino.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ packages/themes/plasma/files/lookandfeel/org.mocaccino.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,9 +1,7 @@
 // Set MocaccinoOS wallpaper on all active desktops
-var allDesktops = desktops();
-for (var i = 0; i < allDesktops.length; i++) {
-    var desktop = allDesktops[i];
-    if (desktop.screen === -1) continue;
+desktops().forEach(function(desktop) {
     desktop.wallpaperPlugin = "org.kde.image";
     desktop.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
     desktop.writeConfig("Image", "file:///usr/share/wallpapers/mocaccino-wallpaper/background.jpg");
-}
+    desktop.reloadConfig();
+});
