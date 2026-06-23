4b2a1ffef8b3f86467297c4be477697bfab76dc1
Joost Ruis <joost.ruis@mocaccino.org>
2026-06-23 15:57:47 +0200

    themes/plasma: bump

--- packages/themes/plasma/files/lookandfeel/org.mocaccino.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ packages/themes/plasma/files/lookandfeel/org.mocaccino.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -7,10 +7,3 @@ for (var i = 0; i < allDesktops.length; i++) {
     desktop.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
     desktop.writeConfig("Image", "file:///usr/share/wallpapers/mocaccino-wallpaper/background.jpg");
 }
-
-// Set panel transparency (0=adaptive, 1=opaque, 2=translucent)
-var allPanels = panels();
-for (var i = 0; i < allPanels.length; i++) {
-    allPanels[i].currentConfigGroup = [];
-    allPanels[i].writeConfig("opacity", 2);
-}
